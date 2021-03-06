3832cc78f0d1ace938eb9bf4175fa753d6e95edc
nrbk <nrbksv@gmail.com>
2021-03-06 12:05:53 +0600

    Model 'Guest' is registered in the admin-panel. Superuser created.

--- guest_book/check_in/admin.py
+++ guest_book/check_in/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from check_in.models import Guest
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'name', 'e_mail', 'create_date', 'update_date']
+    list_filter = ['status']
+    search_fields = ['status', 'name']
+    fields = ['id', 'status', 'name', 'e_mail', 'reg_info', 'create_date', 'update_date']
+    readonly_fields = ['id', 'create_date', 'update_date']
+
+
+admin.site.register(Guest, GuestAdmin)
+
