6d3a318c9bfc9d4b8b35c4616436199b9f84aa19
nrbk <nrbksv@gmail.com>
2021-03-06 13:05:18 +0600

    Added main page for displaying all records in guest book.

--- guest_book/check_in/views.py
+++ guest_book/check_in/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from check_in.models import Guest
+
+
+def guest_list_view(request):
+    guests = Guest.objects.all().order_by('-create_date')
+    return render(request, 'main_page.html', {'guests': guests})
--- guest_book/guest_book/urls.py
+++ guest_book/guest_book/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from check_in.views import guest_list_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', guest_list_view, name='guest-list'),
+
 ]
